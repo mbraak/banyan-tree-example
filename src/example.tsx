b698a6949c348ffe6bb10a39caadb5f2635473be
Marco Braak <mbraak@ridethepony.nl>
2017-05-15 09:54:02 +0200

    initial

---
+++ src/example.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./app";
+import { Tree } from "banyan-react-tree/lib/immutable_tree";
+import example_data from "./dinosaurs.json";
+
+import "banyan-react-tree/banyan-react-tree.css";
+import "./example.css";
+
+const tree = new Tree([example_data])
+    .openLevel(1);
+
+ReactDOM.render(
+    <App tree={tree} />,
+    document.getElementById("tree1")
+);
